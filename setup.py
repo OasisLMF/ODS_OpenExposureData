d9df35545f2c4a49430771cc95eddfa16c20882a
maxwellflitton <maxwellflitton@gmail.com>
2022-07-04 12:22:59 +0100

    updating entrypoints

--- setup.py
+++ setup.py
@@ -57,12 +57,12 @@ setuptools.setup(
     },
     author='Oasis LMF',
     author_email="support@oasislmf.org",
-    # packages=find_packages(where="opends"),
-    # package_dir={
+    packages=find_packages(where="opends"),
+    package_dir={
     #     'ods_tools': 'src',
-    #     'opends': 'opends',
+        '': 'opends',
     #     'opends.adapters': 'opends.adapters'
-    # },
+    },
     python_requires='>=3.7',
     description='Tools to manage ODS files',
     long_description=readme,
