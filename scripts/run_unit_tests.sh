037693d73abcd99dbf1843b62e6bce9762820e98
maxwellflitton <maxwellflitton@gmail.com>
2022-08-03 12:09:54 +0100

    updating requirements

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -5,17 +5,17 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 cd ..
 
-if [ ! -d ./venv ];
-then
-    echo "virtual environment is not present creating venv"
-    python3 -m venv venv
-    echo "venv created"
-    source venv/bin/activate
-    pip install -r requirements.txt
-else
-    echo "virtual environment is already present"
-    source venv/bin/activate
-fi
+#if [ ! -d ./venv ];
+#then
+#    echo "virtual environment is not present creating venv"
+#    python3 -m venv venv
+#    echo "venv created"
+#    source venv/bin/activate
+#    pip install -r requirements.txt
+#else
+#    echo "virtual environment is already present"
+#    source venv/bin/activate
+#fi
 
 export PYTHONPATH="$PWD/src/"
 python -m unittest discover ./tests/
