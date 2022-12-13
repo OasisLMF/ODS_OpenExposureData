b562c1200d99bc7103b80f5472387258d9204ebc
sstruzik <stephane.struzik@gmail.com>
2022-12-13 14:26:48 +0000

    rename dir src to ods_tools

--- docker/build_package.sh
+++ docker/build_package.sh
@@ -12,7 +12,7 @@ fi
 set -e
 # Create Spec Json
     extract_spec.py json --source-excel-path ./OpenExposureData/Docs/OpenExposureData_Spec.xlsx \
-                         --output-json-path ./src/data/OpenExposureData_Spec.json
+                         --output-json-path ./ods_tools/data/OpenExposureData_Spec.json
 
 # Build
     pip install pip-tools wheel 
@@ -20,5 +20,5 @@ set -e
     python setup.py bdist_wheel
 
 # test install 
-    VER_PKG=$(cat ./src/__init__.py | grep  __version__ | awk -F"'" ' {print $2} ')
+    VER_PKG=$(cat ./ods_tools/__init__.py | grep  __version__ | awk -F"'" ' {print $2} ')
     WHL_PKG=$(find ./dist/ -name "ods_tools-${VER_PKG}*.whl")
