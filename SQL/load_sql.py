0d2146f6884c65e7da4f06f0c15b2f1b94f67589
Ben Hayes <ben.hayes@oasislmf.org>
2026-02-02 16:16:49 +0000

    relational model with RI

--- SQL/load_sql.py
+++ SQL/load_sql.py
@@ -1,9 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
 
-
-df_loc = pd.read_csv('SQL_Scripts/SourceFiles/location.csv').head(10)
-
 connection_string = (
     "mssql+pyodbc://sa:password@localhost:1433/OED?"
     "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
@@ -11,6 +8,8 @@ connection_string = (
 
 engine = create_engine(connection_string)
 
+# location
+df_loc = pd.read_csv('SQL_Scripts/SourceFiles/location.csv').head(10)
 df_loc.to_sql(
     name='_staging_location',      # Table name
     con=engine,
@@ -19,9 +18,8 @@ df_loc.to_sql(
     method='multi'            # Faster bulk insert
 )
 
-
+# account
 df_acc = pd.read_csv('SQL_Scripts/SourceFiles/account.csv')
-
 df_acc.to_sql(
     name='_staging_account',      # Table name
     con=engine,
@@ -30,6 +28,27 @@ df_acc.to_sql(
     method='multi'            # Faster bulk insert
 )
 
-with engine.connect() as conn:
-    result = conn.execute(text("EXEC usp_Database_Load"))
-    result
+# reinsurance info
+df_riinfo = pd.read_csv('SQL_Scripts/SourceFiles/ri_info.csv')
+df_riinfo.to_sql(
+    name='_staging_riinfo',      # Table name
+    con=engine,
+    if_exists='replace',      # replace, append, or fail
+    index=False,              # Don't include pandas index
+    method='multi'            # Faster bulk insert
+)
+
+# reinsurance scope
+df_riscope = pd.read_csv('SQL_Scripts/SourceFiles/ri_scope.csv')
+df_riscope.to_sql(
+    name='_staging_riscope',      # Table name
+    con=engine,
+    if_exists='replace',      # replace, append, or fail
+    index=False,              # Don't include pandas index
+    method='multi'            # Faster bulk insert
+)
+
+#with engine.connect() as conn:
+#    result = conn.execute(text("EXEC usp_Database_Load"))
+#    for row in result:
+#        print(row)
